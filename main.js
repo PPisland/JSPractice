d20d4e0c960d4e9fe95144ab3fbc9eeeaf4e94b1
ppisland <a1365gkqls@gmail.com>
2023-03-29 18:02:40 +0900

    날씨API

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const QUOTES = "quotes";
 
+const API_KEY = "cb32e522ca563fad8516248b377b04d9";
+
 function getTime() {
   const time = document.querySelector(".time");
 
@@ -39,7 +41,7 @@ function getQuotes() {
 }
 
 getQuotes();
-setInterval(getQuotes, 1000);
+setInterval(getQuotes, 2000);
 
 function onClickAdd() {
   const newQuotes = document.querySelector(".newQuotes");
@@ -69,35 +71,55 @@ function onClickRegist() {
 let isLoading = false;
 
 async function onClickSearch() {
-  const serachInput = document.querySelector(".searchInput");
-  const serachResult = document.querySelector(".searchResult");
+  const searchInput = document.querySelector(".searchInput");
 
-  if (!serachInput.value) return;
-  if (isLoading) return;
-
-  isLoading = true;
+  if (!searchInput.value) return;
 
   const question = searchInput.value;
-  serachInput.value = "검색 중 입니다... 잠시만  기다려주세요.";
 
+  searchInput.value = "검색 중 입니다... 잠시만 기다려주세요.";
+
+  // 프론트엔드에서 백엔드
   const response = await axios.post(
     "https://holy-fire-2749.fly.dev/chat",
     {
       question,
     },
     {
-      Headers: {
+      headers: {
         "Content-Type": "application/json",
-        Authorization: "Baarer BLOCKCHAINSCHOOL3",
+        Authorization: "Bearer BLOCKCHAINSCHOOL3",
       },
     }
   );
 
-  if (response.status === 200) {
-    serachResult.style.display = "inline";
-    serachResult.innerText = response.data.choices[0].message.content;
-  }
-
-  searchInput.value = "";
-  isLoading = false;
+  console.log(response);
 }
+
+function onClickToggle(value) {
+  const nft = document.querySelector(".nft");
+  const nftView = document.querySelector(".nftView");
+  if (value) {
+    nft.style.display = "inline-block";
+    nftView.style.display = "none";
+  } else {
+    nft.style.display = "none";
+    nftView.style.display = "inline-block";
+  }
+}
+
+const weatherIcon = document.querySelector(".weatherIcon");
+const weatherTemp = document.querySelector(".cityTemp");
+
+navigator.geolocation.getCurrentPosition((position) => {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      weatherTemp.innerText = data.name + ", " + parseInt(data.main.temp) + "℃";
+      weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    });
+});
